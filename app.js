d75b6380116ead9032a54e39a880142a69688a42
Kvitokzy <s.voluevich2017@gmail.com>
2024-04-17 21:21:54 +0300

    create

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const mainRouteController = require("./controllers/main")
+const gameRouteController = require("./controllers/game")
+const defaultRouteController = require("./controllers/default")
+const voteRouteController = require("./controllers/vote")
+// ...импорты и настройки
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    switch (url) {
+      case "/":
+              mainRouteController(res, "/index.html", ".html");
+        break;
+        case "/game":
+            gameRouteController(res);
+      break;
+      case "/vote":
+            voteRouteController(req, res);
+      break;
+        default:
+            defaultRouteController(res, url);
+    }
+  });
+
+  server.listen(3005)
---
+++ controllers/game.js
@@ -0,0 +1,16 @@
+const fs = require("fs").promises
+const { config } = require("../appModules/rating")
+async function gameRouteController(res) {
+  try {
+    const ratingFile = await fs.readFile(config.PATH_TO_RATING_FILE);
+    const data = JSON.parse(ratingFile);
+    const game = data[0];
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(game))
+  } catch (error) {
+    res.statusCode = 500;
+    res.end("Internal Server Error");
+  }
+
+}
+module.exports = gameRouteController;
